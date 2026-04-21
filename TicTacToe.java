6a94799ef61b045947e68aefa2e5efacc1a144b9
Syed Imran <is7920@srmist.edu.in>
2026-04-21 15:54:31 +0530

    uc3 implemented

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,38 +1,18 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    static char currentPlayer;
-    static char player1Symbol;
-    static char player2Symbol;
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        // Perform toss
-        toss();
-
-        // Display result
-        System.out.println("Player 1 Symbol: " + player1Symbol);
-        System.out.println("Player 2 Symbol: " + player2Symbol);
-        System.out.println("First Turn: Player " + (currentPlayer == player1Symbol ? "1" : "2"));
+        int slot = getUserSlot();
+        System.out.println("You selected slot: " + slot);
     }
 
-    public static void toss() {
-        Random rand = new Random();
-
-        // 0 or 1
-        int tossResult = rand.nextInt(2);
-
-        if (tossResult == 0) {
-            // Player 1 starts
-            currentPlayer = 'X';
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            // Player 2 starts
-            currentPlayer = 'X';
-            player2Symbol = 'X';
-            player1Symbol = 'O';
-        }
+    // Method to get user input
+    public static int getUserSlot() {
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
